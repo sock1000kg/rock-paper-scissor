7caa17e55610c26f395575d80be648de774bc7b8
Pham Tat Dat <convitduc6@gmail.com>
2026-09-22 11:40:22 +0700

    chore: configure github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/rock-paper-scissor/',
   plugins: [react()],
   test: {
     environment: 'node',
